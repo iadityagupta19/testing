8e24b3a67c8023a60f63da55e84776831c892458
iadityagupta19 <aditya.gupta@practo.com>
2024-06-26 09:09:43 +0530

    Array List Assignment

---
+++ src/main/java/com/jedi/junit/arrayList/ArrayList.java
@@ -0,0 +1,44 @@
+package com.jedi.junit.arrayList;
+
+import java.util.Arrays;
+
+public class ArrayList<T> {
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+    public void add(T element) {
+        ensureCapacity();
+        elements[size++] = element;
+    }
+
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elements[index];
+    }
+
+    public void remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+}
+
